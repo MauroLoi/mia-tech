4c277c19f64aacf8f79dc5e47d56336ca51fad18
MauroLoi <loimauro@hotmail.com>
2024-12-18 17:38:52 +0100

    added React_ex1

--- Esercitazione/first project/src/App.jsx
+++ Esercitazione/first project/src/App.jsx
@@ -1,44 +1,46 @@
+import { useState } from "react";
 import CustomCard from "./components/CustomCard";
 
-const cards = [
-    {
-        img: "https://images.pexels.com/photos/709552/pexels-photo-709552.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
-        title: "Foresta",
-        text: "questa foresta è una foresta",
-    },
-
-    {
-        img: "https://images.pexels.com/photos/147411/italy-mountains-dawn-daybreak-147411.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
-        title: "Baita sul lago",
-        text: "questa lago è una lago con una baita",
-    },
-
-    {
-        img: "https://images.pexels.com/photos/14092973/pexels-photo-14092973.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
-        title: "Lago e montagne",
-        text: "questo lago è ai piedi delle montagne",
-    },
-
-    {
-        img: "https://images.pexels.com/photos/17217435/pexels-photo-17217435/free-photo-of-acqua-inverno-montagna-ghiaccio.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
-        title: "Lago ghiacciato",
-        text: "questo lago è ghiacciato",
-    },
-
-    {
-        img: "https://images.pexels.com/photos/96387/pexels-photo-96387.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
-        title: "Vulcano",
-        text: "questo è un vulcano",
-    },
-
-    {
-        img: "https://images.pexels.com/photos/8905097/pexels-photo-8905097.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
-        title: "Vulcano in eruzione",
-        text: "questo vulcano sta eruttando",
-    },
-]
-
 const App = () => {
+
+    const [cards, setCards] = useState([
+        {
+            img: "https://images.pexels.com/photos/709552/pexels-photo-709552.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
+            title: "Foresta",
+            text: "questa foresta è una foresta",
+        },
+
+        {
+            img: "https://images.pexels.com/photos/147411/italy-mountains-dawn-daybreak-147411.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
+            title: "Baita sul lago",
+            text: "questa lago è una lago con una baita",
+        },
+
+        {
+            img: "https://images.pexels.com/photos/14092973/pexels-photo-14092973.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
+            title: "Lago e montagne",
+            text: "questo lago è ai piedi delle montagne",
+        },
+
+        {
+            img: "https://images.pexels.com/photos/17217435/pexels-photo-17217435/free-photo-of-acqua-inverno-montagna-ghiaccio.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
+            title: "Lago ghiacciato",
+            text: "questo lago è ghiacciato",
+        },
+
+        {
+            img: "https://images.pexels.com/photos/96387/pexels-photo-96387.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
+            title: "Vulcano",
+            text: "questo è un vulcano",
+        },
+
+        {
+            img: "https://images.pexels.com/photos/8905097/pexels-photo-8905097.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
+            title: "Vulcano in eruzione",
+            text: "questo vulcano sta eruttando",
+        },
+    ]);
+
 
     return (
         <>
