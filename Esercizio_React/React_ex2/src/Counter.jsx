7eb39692753e3a7e2a0db01a0cd23d4b547751b0
MauroLoi <loimauro@hotmail.com>
2025-01-07 13:29:53 +0100

    added ex2 react

---
+++ Esercizio_React/React_ex2/src/Counter.jsx
@@ -0,0 +1,42 @@
+import { useEffect, useState } from "react"
+
+
+const Counter = () => {
+    const [count, setCount] = useState(0);
+
+    const incrementButton = () => {
+        setCount((count) => {
+            return count + 1;
+        })
+    }
+
+    const decrementButton = () => {
+        setCount((count) => {
+            return count - 1;
+        })
+    }
+
+    const resetButton = () => {
+        setCount((count) => {
+            return count = 0;
+        })
+    }
+
+    useEffect (() => {
+        document.title = `Contatore: ${count}`;
+    }, [count]);
+
+    return (
+        <div className="flex justify-center items-center h-screen gap-4 p-10 bg-slate-600">
+            <button className="bg-slate-400 p-2 rounded-2xl text-gray-800 h-12 hover:bg-green-300" 
+                    onClick={incrementButton}>Increment</button>
+            <button className="bg-slate-400 p-2 rounded-2xl text-gray-800 h-12 hover:bg-red-300" 
+                    onClick={decrementButton}>Decrement</button>
+            <button className="bg-slate-400 p-2 rounded-2xl text-gray-800 h-12 hover:bg-yellow-200" 
+                    onClick={resetButton}>Reset</button>
+            <p className="font-bold p-2 text-gray-200">Counter: {count}</p>
+        </div>
+    )
+}
+
+export default Counter
